0f409909d534786b98536f9d6130291a3893de22
GilbertoDev90 <gilberto.dev90@gmail.com>
2025-03-17 10:59:42 -0300

    Foi alterado o css. index e menu

--- menu.js
+++ menu.js
@@ -1,3 +1,11 @@
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        itensmenu.style.display = 'block'
+    } else {
+        itensmenu.style.display = 'none'
+    }
+}
+
 function clickMenu() {
     if (itensmenu.style.display == 'block') {
         itensmenu.style.display = 'none'
